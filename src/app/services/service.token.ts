9ee01678040930d2d7954444174b59f9e9aacdf6
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-03-23 22:29:30 -0300

    fix: add to token and forgot to send yn compoenet

--- src/app/services/service.token.ts
+++ src/app/services/service.token.ts
@@ -1,19 +1,19 @@
 import { InjectionToken } from '@angular/core';
 import { ICLientService } from './api-client/clients/iclients.service';
 import { ISnackbarManagerService } from './isnackbar-manager.service';
-//import { IDialogManagerService } from './idialog-manager.service';
-//import { IScheduleService } from './api-client/schedules/ischedules.service';
+import { IDialogManagerService } from './idialog-manager.service';
+import { IScheduleService } from './api-client/schedules/ischedules.service';
 
 export const SERVICES_TOKEN = {
   HTTP: {
     CLIENT: new InjectionToken<ICLientService>('SERVICES_TOKEN.HTTP.CLIENT'),
-    /*    SCHEDULE: new InjectionToken<IScheduleService>(
+    SCHEDULE: new InjectionToken<IScheduleService>(
       'SERVICES_TOKEN.HTTP.SCHEDULE',
     ),
-    */
+    
   },
   SNACKBAR: new InjectionToken<ISnackbarManagerService>(
     'SERVICES_TOKEN.SNACKBAR',
   ),
-  //  DIALOG: new InjectionToken<IDialogManagerService>('SERVICES_TOKEN.DIALOG'),
+   DIALOG: new InjectionToken<IDialogManagerService>('SERVICES_TOKEN.DIALOG'),
 };
